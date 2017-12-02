f462a77ead9cc4353e535c6bee3f7cfde7860237
Jack Twilley <mathuin@gmail.com>
2017-12-02 12:16:39 -0800

    Solution for Day 1 part 2 in Python

---
+++ Day1b/Day1b.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import sys
+
+
+# Now, instead of considering the next digit, it wants you to consider the
+# digit halfway around the circular list. That is, if your list contains 10
+# items, only include a digit in your sum if the digit 10/2 = 5 steps forward
+# matches it. Fortunately, your list has an even number of elements.
+def captcha(seq):
+    # Split sequence into individual digits.
+    seql = [int(x) for x in seq]
+
+    # Perform the operation on the list
+    sum = 0
+    seqlen = len(seq)
+    step = int(seqlen/2)
+    for i in range(len(seq)):
+        v = seql[i]
+        if v == seql[(i+step) % seqlen]:
+            sum += v
+    return sum
+
+# Unit tests for captcha.
+tt = {'1212': 6, '1221': 0, '123425': 4, '123123': 12, '12131415': 4}
+for k, v in tt.items():
+    result = captcha(k)
+    if result != v:
+        print("FAIL: input ", k, ": expected ", v, ", got ", result, sep="")
+
+# Copy sequence from command line argument.
+# The input is not checked for sanity, just existence.
+if len(sys.argv) != 2:
+    print("Usage:", sys.argv[0], "[sequence]")
+    sys.exit(1)
+seq = sys.argv[1]
+
+print(captcha(seq))
