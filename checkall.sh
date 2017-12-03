a52e74cc5efb6d27530b27288d2f517484506552
Jack Twilley <mathuin@gmail.com>
2017-12-03 13:34:44 -0800

    Minor tweak to checkall.sh
    It will only try to run languages that are found.

--- checkall.sh
+++ checkall.sh
@@ -3,21 +3,30 @@
 tmpout="$(mktemp -d /tmp/advent.XXXXXX)"
 for dir in `ls -d */`; do
     basename=`basename $dir`
-    echo "$basename:"
     pushd $dir >/dev/null
-    for language in Python Go; do
-        if [ $language == "Python" ]; then
-            ./$basename.py < input > $tmpout/checkout
-        elif [ $language == "Go" ]; then
-            go run ./$basename.go < input > $tmpout/checkout
-        fi
-        diff $tmpout/checkout output >/dev/null
-        if [ $? -eq 0 ]; then
-            echo "  $language PASS"
-        else
-            echo "  $language FAIL"
-        fi
-    done
+    languages=""
+    if [ -f $basename.py ]; then
+        languages="$languages Python"
+    fi
+    if [ -f $basename.go ]; then
+        languages="$languages Go"
+    fi
+    if [ "$languages" != "" ]; then
+        echo "$basename:"
+        for language in $languages; do
+            if [ $language == "Python" ]; then
+                ./$basename.py < input > $tmpout/checkout
+            elif [ $language == "Go" ]; then
+                go run ./$basename.go < input > $tmpout/checkout
+            fi
+            diff $tmpout/checkout output >/dev/null
+            if [ $? -eq 0 ]; then
+                echo "  $language PASS"
+            else
+                echo "  $language FAIL"
+            fi
+        done
+    fi
     popd >/dev/null
 done
 rm -rf $tmpout
