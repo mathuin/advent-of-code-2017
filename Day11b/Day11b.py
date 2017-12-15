f00850235c58d33b12077dd98e73209bc94ad79a
Jack Twilley <mathuin@gmail.com>
2017-12-14 19:24:59 -0800

    Solution to Day 11 part 2 in Python

---
+++ Day11b/Day11b.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python3
+
+import sys
+
+# hex ed
+
+# n, ne, se, s, sw, nw
+
+# given path child took, calculate fewest steps to reach them.
+
+# 1. remove opposing steps (n<-->s)
+# 2. merge two-away steps (n + se = ne, etc)
+# 3. remove opposing steps again?
+
+stepOrder = ['n', 'ne', 'se', 's', 'sw', 'nw']
+
+
+def removeOpposing(path):
+    half = len(stepOrder)//2
+
+    for dir in range(0, half):
+        cancelled = min(path[stepOrder[dir]], path[stepOrder[dir+half]])
+        path[stepOrder[dir]] -= cancelled
+        path[stepOrder[dir+half]] -= cancelled
+
+    return path
+
+
+def mergeSemiAdjacent(path):
+    numSteps = len(stepOrder)
+
+    for dir in range(0, numSteps):
+        merged = min(path[stepOrder[dir]], path[stepOrder[(dir+2) % numSteps]])
+        path[stepOrder[dir]] -= merged
+        path[stepOrder[(dir+1) % numSteps]] += merged
+        path[stepOrder[(dir+2) % numSteps]] -= merged
+
+    return path
+
+
+def calculateFarthest(stepstr):
+    path = {key: 0 for key in stepOrder}
+    farthest = 0
+
+    for step in stepstr.rstrip().split(','):
+        path[step] += 1
+        currdist = getShortestPath(path)
+        if currdist > farthest:
+            farthest = currdist
+
+    return farthest
+
+
+def lenPath(path):
+    return sum(path[dir] for dir in stepOrder)
+
+
+def getShortestPath(path):
+    oldpath = 999999999
+    while True:
+        path = removeOpposing(path)
+        path = mergeSemiAdjacent(path)
+        lenpath = lenPath(path)
+        if lenpath == oldpath:
+            break
+        oldpath = lenpath
+    return lenpath
+
+
+tt = {
+    'ne,ne,ne': 3,
+    'ne,ne,sw,sw': 2,
+    'ne,ne,s,s': 2,
+    'se,sw,se,sw,sw': 3
+}
+for k, v in tt.items():
+    result = calculateFarthest(k)
+    if result != v:
+        print("FAIL: input ", k, ": expected ", v, ", got ", result, sep="")
+
+# The input is not checked for sanity, just existence.
+stepstrs = sys.stdin.readlines()
+if len(stepstrs) == 0:
+    print("stepstrs missing!")
+    sys.exit(1)
+
+print(calculateFarthest(stepstrs[0]))
